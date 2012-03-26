46d24fcec9fad07452abb67e75dab1eb0b3576b2
John Hurliman <jhurliman@jhurliman.org>
2012-03-26 01:34:21 -0700

    Added gain.js and waveform.js example tools

---
+++ examples/gain.js
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * Calculate the ReplayGain adjustment for a file containing an audio track.
+ * See http://replaygain.org/ for more information.
+ * http://github.com/jhurliman/node-pcm
+ * Copyright (c) John Hurliman <jhurliman@cull.tv>. 2012
+ *****************************************************************************/
+
+var fs = require('fs');
+var pcm = require('../lib/pcm');
+var ReplayGain = require('../misc/replayGain');
+
+var SAMPLE_RATE = 11025;
+var CHANNELS = 2;
+
+// Check command line args
+if (process.argv.length !== 3) {
+  console.error('Usage: node gain.js [input]. Input is anyfile with an audio ' +
+    'track readable by ffmpeg');
+  process.exit(-1);
+}
+
+var gainleftbuf = [];
+var gainrightbuf = [];
+
+var replayGain = new ReplayGain(SAMPLE_RATE);
+
+console.log('Reading audio data...');
+pcm.getPcmData(process.argv[2], { stereo: CHANNELS === 2, sampleRate: SAMPLE_RATE },
+  function(sample, channel) {
+    sample *= 32767.0;
+    
+    if (channel === 0)
+      gainleftbuf.push(sample);
+    else
+      gainrightbuf.push(sample);
+    
+    if (gainleftbuf.length === ReplayGain.MIN_BUFSIZE && gainrightbuf.length === ReplayGain.MIN_BUFSIZE) {
+      replayGain.analyzeSamples(gainleftbuf, gainrightbuf);
+      gainleftbuf.length = 0;
+      gainrightbuf.length = 0;
+    }
+  },
+  function(err, output) {
+    if (err) throw err;
+    
+    var gain = replayGain.getReplayGain();
+    console.log('ReplayGain = ' + gain.toFixed(2) + 'dB');
+  }
+);
---
+++ examples/waveform.js
@@ -0,0 +1,126 @@
+/******************************************************************************
+ * Generate a PNG waveform image from a file containing an audio track.
+ * http://github.com/jhurliman/node-pcm
+ * Copyright (c) John Hurliman <jhurliman@cull.tv>. 2012
+ *****************************************************************************/
+
+var fs = require('fs');
+var pcm = require('../lib/pcm');
+var Canvas;
+var ffprobe;
+
+var WIDTH = 1800;
+var HEIGHT = 280;
+var SAMPLE_RATE = 11025;
+var CHANNELS = 2;
+
+// Check command line args
+if (process.argv.length !== 4) {
+  console.error('Usage: node waveform.js [input] [output.png]. Input is any ' +
+    'file with an audio track readable by ffmpeg, and output is a ' +
+    WIDTH+'x'+HEIGHT + ' PNG waveform image');
+  process.exit(-1);
+}
+
+// Test if Canvas is installed
+try {
+  Canvas = require('canvas');
+  ffprobe = require('node-ffprobe');
+} catch (ex) {
+  console.error('Canvas and node-ffprobe are required. Run "npm install canvas" ' +
+    'and "npm install node-ffprobe" first.');
+  process.exit(-2);
+}
+
+var peaks = new Array(WIDTH); // Peak value for each pixel of the output image
+var peakIdx = 0; // Current index into peak array
+var totalMax = 0; // Highest value seen in the input
+var curMax = 0; // Highest value seen for the current round
+var sampleIdx = 0; // Current sample index
+
+ffprobe(process.argv[2], function(inputInfo) {
+  // Calculate the number of samples to merge into each vertical column of pixels
+  // in the output image
+  var samplesPerPeak = Math.round(
+    inputInfo.format.duration * SAMPLE_RATE / WIDTH) * CHANNELS;
+  
+  console.log('Reading audio data...');
+  pcm.getPcmData(process.argv[2], { stereo: CHANNELS === 2, sampleRate: SAMPLE_RATE },
+    function(sample, channel) {
+      sample = Math.abs(sample);
+      if (sample > curMax) curMax = sample;
+      
+      // Do we have enough samples to store a peak value?
+      if (++sampleIdx >= samplesPerPeak)
+        storePeak();
+    },
+    function(err, output) {
+      if (err) throw err;
+      
+      // Store the final peak value if needed
+      if (sampleIdx > 0 && peakIdx < peaks.length)
+        storePeak();
+      
+      console.log('Creating output image...');
+      writePNG();
+      process.on('exit', function() { console.log('Done'); });
+    }
+  );
+});
+
+function storePeak() {
+  if (curMax > 0)
+    curMax = altLogMeter(coefficientTodB(curMax));
+  else
+    curMax = -altLogMeter(coefficientTodB(-curMax));
+  peaks[peakIdx++] = curMax;
+  
+  if (curMax > totalMax)
+    totalMax = curMax;
+  
+  curMax = 0;
+  sampleIdx = 0;
+}
+
+function log10(arg) {
+  return Math.log(arg) / Math.LN10;
+}
+
+function logMeter(power, lowerdB, upperdB, nonLinearity) {
+  return (power < lowerdB ? 0 : Math.pow((power - lowerdB) / (upperdB - lowerdB), nonLinearity));
+}
+
+function altLogMeter(power) {
+  return logMeter(power, -192.0, 0.0, 8.0);
+}
+
+function coefficientTodB(coeff) {
+  return 20.0 * log10(coeff);
+}
+
+function writePNG() {
+  var canvas = new Canvas(1800, 280);
+  var ctx = canvas.getContext('2d');
+  
+  var gain = 1.0 / totalMax;
+  
+  for (var i = 0; i < peaks.length; i++)
+    drawLine(ctx, i, null, peaks[i] * gain);
+  
+  var out = fs.createWriteStream(process.argv[3]);
+  var stream = canvas.createPNGStream();
+  stream.pipe(out);
+}
+
+function drawLine(ctx, offset, min, max) {
+  ctx.strokeStyle = 'white';
+  ctx.beginPath();
+  ctx.moveTo(offset, 0);
+  ctx.lineTo(offset, 140 - 140 * max);
+  ctx.stroke();
+  
+  ctx.beginPath();
+  ctx.moveTo(offset, 280);
+  ctx.lineTo(offset, 140 + 140 * max);
+  ctx.stroke();
+}
